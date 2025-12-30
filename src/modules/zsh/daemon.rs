c8da59503203a9b035e88a5920ee7593b7d7117b
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-12-30 11:31:14 +0900

    feat: add some item

--- src/modules/zsh/daemon.rs
+++ src/modules/zsh/daemon.rs
@@ -1,3 +1,21 @@
 use crate::args::DaemonCommands;
 
-pub async fn main(command: DaemonCommands) {}
+mod start;
+mod stop;
+use start::start;
+use stop::stop;
+
+pub async fn main(command: DaemonCommands) {
+    match command {
+        DaemonCommands::Restart => {
+            stop().await;
+            start().await;
+        }
+        DaemonCommands::Start => {
+            start().await;
+        }
+        DaemonCommands::Stop => {
+            stop().await;
+        }
+    }
+}
